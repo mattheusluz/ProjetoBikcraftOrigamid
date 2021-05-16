9f4cf7f80d76bd03458cb79894acf534ac787b22
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2021-05-16 15:36:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ if(window.SimpleForm){
 new SimpleForm({
   form: ".formphp", // seletor do formulário
   button: "#enviar", // seletor do botão
-  erro: "<div id='form-erro'><p>Um erro ocorreu, tente para o email mattheusluzz@gmail.com.</p></div>", // mensagem de erro
+  erro: "<div id='form-erro'><p>Um erro ocorreu, mande um email para mattheusluzz@gmail.com.</p></div>", // mensagem de erro
   sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
 });
 }
