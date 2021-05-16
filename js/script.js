d4c4defb8a01bbeba07d30e6cf5ff27bcb55ba99
mattheusluz <80133489+mattheusluz@users.noreply.github.com>
2021-05-16 11:06:10 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+if(window.SimpleSlide){
+  
+new SimpleSlide({
+  slide: 'quote', // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+})
+
+new SimpleSlide({
+  slide: 'portfolio', // nome do atributo data-slide="principal"
+  time: 5000, // tempo de transição dos slides
+  pauseOnHover: true, // pausa a transição automática
+  nav: true
+})
+
+}
